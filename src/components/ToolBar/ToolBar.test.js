7d64dfaa19930c96a8ed39ede7a12efb6fcfe187
Roy <technicmk@gmail.com>
2017-12-14 00:04:15 +0200

    Update test

--- src/components/ToolBar/ToolBar.test.js
+++ src/components/ToolBar/ToolBar.test.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import {spy} from 'sinon'
 import toJson from 'enzyme-to-json'
 
 import ToolBar from './'
 
-it("It should run onIconClick when the Icon is clicked", () =>{
-	const Icon = () => <span>Icon</span>
+const Icon = () => <span>Icon</span>
+
+it("It renders without crashing", () => {
+	const div = document.createElement('div')
+	ReactDOM.render(<ToolBar Icon={Icon} />, div)
+})
+
+
+it("It calls onIconClick when the Icon is clicked", () =>{
 	const onClick = spy()
 	const wrapper = shallow(<ToolBar Icon={Icon} onIconClick={onClick} />)
-	expect(wrapper.contains(<Icon />)).toBe(true)
 	wrapper.find('.-toolbar-icon').simulate('click')
 	expect(onClick.called).toBe(true)
 })
