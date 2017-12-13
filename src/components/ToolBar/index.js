e262806a9504e4e7acfb9901e8179e353625ffd0
Roy <technicmk@gmail.com>
2017-12-13 17:21:54 +0200

    Fix up Toolbar

--- src/components/ToolBar/index.js
+++ src/components/ToolBar/index.js
@@ -2,21 +2,27 @@
 import React from 'react'
 import './style.scss'
 
-// {icon && <IconButton className="-toolbar-icon" />}
+
 const ToolBar = ({style, Icon, onIconClick, Left, Right}) => (
 	<div style={style} className="-toolbar">
-		{Icon && (
+		{Icon &&
 			<div onClick={onIconClick}
 				className="-toolbar-icon">
 				<Icon />
 			</div>
-		)}
-		<div className="-toolbar-left">
-			<Left/>
-		</div>
-		<div className="-toolbar-right">
-			<Right/>
-		</div>
+		}
+
+		{Left &&
+			<div className="-toolbar-left">
+				<Left/>
+			</div>
+		}
+
+		{Right &&
+			<div className="-toolbar-right">
+				<Right/>
+			</div>
+		}
 	</div>
 )
 
