8082cf053eabde561d844edbbc910ea784af16a8
Roy <technicmk@gmail.com>
2017-12-14 14:43:52 +0200

    Import that global style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'tachyons'
 
 import Root from './containers/Root'
 import registerServiceWorker from './registerServiceWorker'
-import 'tachyons'
+import './index.scss'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
 registerServiceWorker()
