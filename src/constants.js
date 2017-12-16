4dd7bf5069dc485c85683bc3753803d4d8b71970
Roy <technicmk@gmail.com>
2017-12-16 22:01:45 +0200

    Add some color constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,6 @@
 export const appName = 'Octopalm'
+export const colors = [
+	'#673ab7', '#cddc39', '#009688', '#2196f3',
+	'#607d8b', '#795548', '#ffc107', '#ff5722',
+	'#00bcd4', '#3f51b5', '#9c27b0', '#9e9e9e'
+]
