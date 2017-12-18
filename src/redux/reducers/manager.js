75c9911f3a9047cc6d3eae5eab64e21ddb9689d9
Roy <technicmk@gmail.com>
2017-12-18 22:25:13 +0200

    Create manager reducer

--- src/redux/reducers/manager.js
+++ src/redux/reducers/manager.js
@@ -2,21 +2,57 @@
 const initialState = {
 	loading: false,
 	error: null,
-	token: null,
-	profile:{
-		id: null,
-		name: null,
-		student_id: null,
-		active_timetable_id: null,
-		module_ids: [],
-		timetable_ids: [],
-		timetable_aliases: []
-	}
+	timetable: {}
 }
 
-export default (state=initialState, {type}) => {
+export default function(state=initialState, {type, payload}){
 	switch (type){
+		case 'ADD_EVENT':
+			return {
+				...state,
+				timetable: {
+					...state.timetable,
+					events: state.timetable.events.concat(payload)
+				}
+			}
 
+		case 'REMOVE_EVENT':
+			return {
+				...state,
+				timetable: {
+					...state.timetable,
+					events: state.timetable.events.filter(e => e.id !== payload)
+				}
+			}
+		case 'LOAD_TABLE':
+			return  {
+				...state,
+				timetable: payload
+			}
+
+		case 'SYNC_TABLE_REQ':
+			return {
+				...state,
+				loading: true
+			}
+		case 'SYNC_TABLE_SUCCESS':
+			return {
+				...state,
+				loading: false,
+				error: null
+			}
+		case 'SYNC_TABLE_FAIL':
+			return {
+				...state,
+				loading: false,
+				error: payload
+			}
+		case 'CLEAR_ERROR':
+			return {
+				...state,
+				error: null
+			}
 	}
+
 	return state
 }
