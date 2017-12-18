6c3fe0886b169f141bcad66d5d396a9df8b871a3
Roy <technicmk@gmail.com>
2017-12-18 17:11:40 +0200

    Update test

--- src/components/ToolBar/test.js
+++ src/components/ToolBar/test.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import {spy} from 'sinon'
 import ToolBar from './'
 
-
-const Icon = () => <span>Icon</span>
-
-it("It renders without crashing", () => {
-	const div = document.createElement('div')
-	ReactDOM.render(<ToolBar Icon={<Icon />} />, div)
-})
-
+renderTest(<ToolBar/>)
 
 it("It calls onIconClick when the Icon is clicked", () =>{
 	const onClick = spy()
-	const wrapper = shallow(<ToolBar Icon={<Icon />} onIconClick={onClick} />)
+	const wrapper = shallow(<ToolBar Icon={<span />} onIconClick={onClick} />)
 	wrapper.find('.-toolbar-icon').simulate('click')
 	expect(onClick.called).toBe(true)
 })
